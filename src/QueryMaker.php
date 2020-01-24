f47ddf0b5361dd9de36a5efe4b54f48237376f1b
midorikocak <mtkocak@gmail.com>
2020-01-24 11:02:49 +0100

    fix style

--- src/QueryMaker.php
+++ src/QueryMaker.php
@@ -19,118 +19,118 @@ class QueryMaker implements QueryInterface
 
     private function __construct()
     {
-        $this->query = '';
+        $this->query     = '';
         $this->statement = '';
-        $this->params = [];
+        $this->params    = [];
     }
 
-    public static function select($table, array $columns = ['*']): QueryInterface
+    public static function select($table, array $columns = ['*']) : QueryInterface
     {
-        $instance = new QueryMaker();
-        $columnsText = implode(', ', $columns);
-        $instance->statement = 'SELECT '.$columnsText.' FROM '.$table;
-        $instance->query = 'SELECT '.$columnsText.' FROM '.$table;
+        $instance            = new QueryMaker();
+        $columnsText         = implode(', ', $columns);
+        $instance->statement = 'SELECT ' . $columnsText . ' FROM ' . $table;
+        $instance->query     = 'SELECT ' . $columnsText . ' FROM ' . $table;
         return $instance;
     }
 
-    public static function update($table, array $values): QueryInterface
+    public static function update($table, array $values) : QueryInterface
     {
-        $instance = new QueryMaker();
-        $instance->statement = 'UPDATE '.$table.' SET ';
-        $instance->query = 'UPDATE '.$table.' SET ';
+        $instance            = new QueryMaker();
+        $instance->statement = 'UPDATE ' . $table . ' SET ';
+        $instance->query     = 'UPDATE ' . $table . ' SET ';
         $instance->prepareParams($values, ', ');
         return $instance;
     }
 
-    public static function delete($table): QueryInterface
+    public static function delete($table) : QueryInterface
     {
-        $instance = new QueryMaker();
-        $instance->statement = 'DELETE FROM '.$table;
-        $instance->query = 'DELETE FROM '.$table;
+        $instance            = new QueryMaker();
+        $instance->statement = 'DELETE FROM ' . $table;
+        $instance->query     = 'DELETE FROM ' . $table;
         return $instance;
     }
 
-    public function where($key, $value): QueryInterface
+    public function where($key, $value) : QueryInterface
     {
-        $operator = $this->getOperator($value);
-        $this->statement .= ' WHERE '.$key.$operator.':'.$key;
-        $this->query .= ' WHERE '.$key.$operator.'\''.$value.'\'';
+        $operator           = $this->getOperator($value);
+        $this->statement   .= ' WHERE ' . $key . $operator . ':' . $key;
+        $this->query       .= ' WHERE ' . $key . $operator . '\'' . $value . '\'';
         $this->params[$key] = $value;
         return $this;
     }
 
-    public function and($key, $value): QueryInterface
+    public function and($key, $value) : QueryInterface
     {
-        $this->query .= " AND ";
+        $this->query     .= " AND ";
         $this->statement .= " AND ";
         $this->prepareParam($key, $value, 'AND');
         return $this;
     }
 
-    public function or($key, $value): QueryInterface
+    public function or($key, $value) : QueryInterface
     {
-        $this->query .= " OR ";
+        $this->query     .= " OR ";
         $this->statement .= " OR ";
         $this->prepareParam($key, $value, 'OR');
         return $this;
     }
 
-    public function between($key, $before, $after): QueryInterface
+    public function between($key, $before, $after) : QueryInterface
     {
-        $this->query .= $key." BETWEEN $before AND $after";
-        $this->statement .= $key.' BETWEEN :before AND :after';
+        $this->query     .= $key . " BETWEEN $before AND $after";
+        $this->statement .= $key . ' BETWEEN :before AND :after';
 
         $this->params['before'] = $before;
-        $this->params['after'] = $after;
+        $this->params['after']  = $after;
         return $this;
     }
 
-    public function getQuery(): string
+    public function getQuery() : string
     {
         return $this->query;
     }
 
-    public function getStatement(): string
+    public function getStatement() : string
     {
         return $this->statement;
     }
 
-    public function getParams(): array
+    public function getParams() : array
     {
         return $this->params;
     }
 
     private function prepareParams(array $values, string $glue)
     {
-        $params = [];
+        $params      = [];
         $queryValues = [];
 
         foreach ($values as $key => $value) {
             $operator = $this->getOperator($value);
-            if (!isset($this->params[$key])) {
-                $queryValues[] = $key.$operator.'\''.$value.'\'';
-                $params [] = $key.$operator.':'.$key;
+            if (! isset($this->params[$key])) {
+                $queryValues[] = $key . $operator . '\'' . $value . '\'';
+                $params []     = $key . $operator . ':' . $key;
 
                 $this->params[$key] = $value;
             } else {
-                $uniqid = uniqid('', true);
-                $queryValues[] = $key.$operator.'\''.$value.'\'';
-                $params [] = $key.$operator.':'.$key.$uniqid;
+                $uniqid        = uniqid('', true);
+                $queryValues[] = $key . $operator . '\'' . $value . '\'';
+                $params []     = $key . $operator . ':' . $key . $uniqid;
 
-                $this->params[$key.$uniqid] = $value;
+                $this->params[$key . $uniqid] = $value;
             }
         }
 
-        $this->query .= implode($glue, $queryValues);
+        $this->query     .= implode($glue, $queryValues);
         $this->statement .= implode($glue, $params);
     }
 
     private function getOperator(&$value)
     {
-        $hasOperator = preg_match('~^(([<>=])+(=)*)~', (string)$value, $matches);
-        if (!empty($hasOperator)) {
+        $hasOperator = preg_match('~^(([<>=])+(=)*)~', (string) $value, $matches);
+        if (! empty($hasOperator)) {
             $operator = reset($matches);
-            $value = substr($value, strlen($operator));
+            $value    = substr($value, strlen($operator));
         } else {
             $operator = '=';
         }
